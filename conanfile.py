1d0e4fa93bfbf9c05407c2f75cb8bad671dcd598
Josiah Ebhomenye <joebhomenye@gmail.com>
2026-05-14 02:06:13 +0100

    major version update to 1.0.0

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
 class VulkanizerConan(ConanFile):
     name = "vulkanizer"
-    version = "0.1.0"
+    version = "1.0.0"
     package_type = "static-library"
 
     license = "MIT"
