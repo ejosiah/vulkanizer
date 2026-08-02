b685df326e545c8f152ec8884e7fb901a76b2487
Josiah Ebhomenye <joebhomenye@gmail.com>
2026-08-02 21:12:03 +0100

    git url to conan file

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.scm import Git
 
 
 class VulkanizerConan(ConanFile):
@@ -8,16 +9,13 @@ class VulkanizerConan(ConanFile):
     package_type = "static-library"
 
     license = "MIT"
+    url = "https://github.com/ejosiah/vulkanizer"
     description = "Small Vulkan helper library with GLFW, ImGui, VMA, GLM, and Volk integration."
     topics = ("vulkan", "imgui", "glfw")
 
     settings = "os", "compiler", "build_type", "arch"
-    exports_sources = (
-        "CMakeLists.txt",
-        "cmake/*",
-        "include/*",
-        "src/*",
-    )
+    source_url = "https://github.com/ejosiah/vulkanizer.git"
+    source_tag = f"v{version}"
 
     def requirements(self):
         self.requires("volk/1.3.296.0")
@@ -25,6 +23,11 @@ class VulkanizerConan(ConanFile):
         self.requires("imgui/1.92.7")
         self.requires("glm/1.0.1")
         self.requires("vulkan-memory-allocator/3.3.0")
+
+    def source(self):
+        git = Git(self)
+        git.clone(url=self.source_url, target=".")
+        git.checkout(self.source_tag)
 
     def layout(self):
         cmake_layout(self)
